e959300ce8d3e81f7e62cebc62fed606fa3d7d17
Bikash Kumar <Kbikash845@gmail.com>
2024-03-08 20:39:17 -0800

    rendom

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Practice from './Components/Practice';
+
+function App() {
+  return (
+   <div>
+    <Practice/>
+   </div>
+  );
+}
+
+export default App;
